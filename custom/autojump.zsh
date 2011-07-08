7f3d48f9f76323ae377668207190dc55a62ab053
Locojay <Locojay@dev.dev>
2011-07-08 17:59:17 -0400

    fix autojump for linux

--- custom/autojump.zsh
+++
@@ -1,7 +0,0 @@
-if [[ "$(uname)" = "Darwin" ]] ; then
-    if [ -f `brew --prefix`/etc/autojump ]; then
-        . `brew --prefix`/etc/autojump
-    fi
-elif [[ "$(uname)" = "Linux" ]] ; then
-    source /etc/profile.d/autojump.zsh
-fi
