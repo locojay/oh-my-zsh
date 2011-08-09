be4099399d53370b2d0d26a53e8671d3870df3a7
locojay <locojay@air.com>
2011-08-09 12:28:13 -0400

    addin plugin function

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -15,4 +15,17 @@ function take() {
   cd $1
 }
 
+function zsh-theme(){
+  echo $ZSH/themes/$1
+  source $ZSH/themes/$1
+  typeset -ga chpwd_functions
+  typeset -ga precmd_functions
+  precmd_functions=''
+}
+compctl -k "(`\ls $ZSH/themes`)" zsh-theme
 
+function zsh-plugin(){
+  echo $ZSH/plugins/$1
+  source $ZSH/plugins/$1/$1.plugin.zsh
+}
+compctl -k "(`\ls $ZSH/plugins`)" zsh-plugin
