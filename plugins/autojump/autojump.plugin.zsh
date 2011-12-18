d5ca05d71bf1f41a19b57fa1097179c924719bcc
LocJayMP <locojaymp@dev>
2011-12-17 19:34:25 -0500

    fixing autojump

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-#if [ -f `brew --prefix`/etc/autojump ]; then
-  #. `brew --prefix`/etc/autojump
-#fi
-=======
 if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
   . /opt/local/etc/profile.d/autojump.sh
 elif [ -f `brew --prefix`/etc/autojump ]; then
   . `brew --prefix`/etc/autojump
 fi
->>>>>>> robbrussel
