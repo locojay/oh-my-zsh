2a6af63d4283a567170b1621e3aebf2f9ed17404
locojaydev <locojaydev@dev.com>
2012-02-06 12:29:49 -0500

    updating boostrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #getting submodules
 git submodule init
 git submodule update
-sudo brew install autojump
-sudo brew install source-highlight
-f=$(pwd)
-ln -Fs $f"/zshrc" ~/.zshrc
+#sudo brew install autojump
+#sudo brew install source-highlight
+ohmyzshf=$(pwd)
+
+SYMLINK="ln -fs"
+
+$SYMLINK $ohmyzshf/zshrc $HOME/.zshrc
+$SYMLINK $ohmyzshf $HOME/.oh-my-zsh
