a82430f8b4064ff91a38b4ce083dc46c7d20b9a9
locojaydev <locojaydev@dev.com>
2012-02-08 18:57:39 -0500

    updating bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,4 @@
 #getting submodules
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
 #sudo brew install autojump
 #sudo brew install source-highlight
 ohmyzshf=$(pwd)
@@ -9,3 +6,7 @@ ohmyzshf=$(pwd)
 SYMLINK="ln -nfs"
 
 $SYMLINK $ohmyzshf/zshrc $HOME/.zshrc
+
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
